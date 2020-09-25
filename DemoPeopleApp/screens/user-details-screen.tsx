45afc81f68ef664935776b17db8418aac7130846
Larry A. Hendrix <lahendrix@uwalumni.com>
2020-09-25 12:08:48 -0500

    Make phone and address clickable.

--- DemoPeopleApp/screens/user-details-screen.tsx
+++ DemoPeopleApp/screens/user-details-screen.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Linking, Platform} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import styles from './user-details.styles';
 
 class UserDetails extends React.Component {
+  dialCall = (phone) => {
+    let phoneNumber = '';
+    if (Platform.OS !== 'android') {
+      phoneNumber = `tel:${phone}`;
+    } else {
+      phoneNumber = `telprompt:${phone}`;
+    }
+    return Linking.openURL(phoneNumber);
+  };
+
+  openMaps = ({lat, lng, userName}) => {
+    const scheme = Platform.select({ios: 'maps:0,0?q=', android: 'geo:0,0?q='});
+    const latLng = `${lat},${lng}`;
+    const url = Platform.select({
+      ios: `${scheme}${userName}@${latLng}`,
+      android: `${scheme}${latLng}(${userName})`,
+    });
+
+    return Linking.openURL(url);
+  };
+
   render() {
     const {userDetails} = this.props.route.params;
 
@@ -10,22 +32,35 @@ class UserDetails extends React.Component {
       <View style={styles.container}>
         <Text style={styles.title}>{userDetails.name}</Text>
         <Text style={styles.subTitle}>{userDetails.company.name}</Text>
-        <Text style={styles.subTitle}>Contact Information</Text>
-        <Text>{userDetails.email}</Text>
+        <Text style={styles.sectionHeader}>Contact Information</Text>
+        <Text style={styles.sectionText}>{userDetails.email}</Text>
         <View>
-          <Text>{userDetails.address.street}</Text>
-          {userDetails.address.suite && (
-            <Text>{userDetails.address.suite}</Text>
-          )}
-          <Text>
-            {userDetails.address.city}, {userDetails.address.zipcode}
-          </Text>
-          <Text>{userDetails.phone}</Text>
+          <TouchableOpacity
+            onPress={() => {
+              this.openMaps({
+                ...userDetails.address.geo,
+                userName: userDetails.name,
+              });
+            }}>
+            <Text style={styles.sectionText}>{userDetails.address.street}</Text>
+            {userDetails.address.suite && (
+              <Text style={styles.sectionText}>
+                {userDetails.address.suite}
+              </Text>
+            )}
+            <Text style={styles.sectionText}>
+              {userDetails.address.city}, {userDetails.address.zipcode}
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => this.dialCall(userDetails.phone)}>
+            <Text style={styles.sectionText}>{userDetails.phone}</Text>
+          </TouchableOpacity>
         </View>
 
-        <Text style={styles.subTitle}>Other Information</Text>
-        <Text>Username: {userDetails.username}</Text>
-        <Text>Website: {userDetails.website}</Text>
+        <Text style={styles.sectionHeader}>Other Information</Text>
+        <Text style={styles.sectionText}>Username: {userDetails.username}</Text>
+        <Text style={styles.sectionText}>Website: {userDetails.website}</Text>
       </View>
     );
   }
--- DemoPeopleApp/screens/user-details.styles.ts
+++ DemoPeopleApp/screens/user-details.styles.ts
@@ -1,22 +1,33 @@
 import {StyleSheet} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
+import {Colors, Fonts, Metrics} from '../themes';
 export default StyleSheet.create({
   container: {
     marginTop: 32,
     paddingHorizontal: 24,
   },
   title: {
-    fontSize: 24,
+    fontSize: Fonts.size.h2,
     fontWeight: '600',
-    color: Colors.black,
+    color: Colors.charcoal,
     textAlign: 'center',
   },
   subTitle: {
     marginTop: 8,
-    fontSize: 18,
+    fontSize: Fonts.size.h4,
     fontWeight: '400',
     color: 'gray',
     textAlign: 'center',
+  },
+  sectionHeader: {
+    marginTop: Metrics.doubleBaseMargin,
+    marginBottom: Metrics.baseMargin,
+    fontSize: Fonts.size.h5,
+    fontWeight: '400',
+    color: Colors.facebook,
+  },
+  sectionText: {
+    marginTop: Metrics.baseMargin,
+    fontSize: Fonts.size.input,
+    color: Colors.coal,
   },
 });
